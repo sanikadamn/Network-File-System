09d3b0dd6c9e23d436b6127130ff820373592945
aryanchandramania <arashaurchandramania@gmail.com>
2023-11-20 13:20:48 +0530

    do almost nothing

--- Client/client.c
+++ Client/client.c
@@ -1,40 +1,40 @@
 #include "includes.h"
 
-int main()
-{
-    // start the client
-    int client_socket;
-    struct sockaddr_in client_addr;
+struct sockaddr_in client_addr;
 
-    client_socket = socket(AF_INET, SOCK_STREAM, 0);
+int init_connection(char *ip, int port)
+{
+    int client_socket = socket(AF_INET, SOCK_STREAM, 0);
     if(client_socket < 0)
     {
-        perror("socket");
+        perror("[-] socket error");
         exit(0);
     }
     else
-        printf("Socket created successfully.\n");
+        printf("[+] client socket created.\n");
 
     bzero(&client_addr, sizeof(client_addr));
     client_addr.sin_family = AF_INET;
-    client_addr.sin_port = htons(DEFAULT_CLIENT_PORT);
-    client_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    client_addr.sin_port = htons(port);
+    client_addr.sin_addr.s_addr = inet_addr(ip);
 
-    // binding client
-    if(bind(client_socket, (struct sockaddr*)&client_addr, sizeof(client_addr)) < 0)
+    int cnnct_ret = connect(client_socket, (struct sockaddr*)&client_addr, sizeof(client_addr));
+    if(cnnct_ret < 0)
     {
-        perror("bind");
+        perror("[-] connect error");
         exit(0);
     }
     else
-        printf("Socket bound to address and port.\n");
+        printf("[+] connected to the server.\n");
 
-    if (listen(client_socket, 100) < 0)
-    {
-        perror("listen");
-        exit(0);
-    }
-    else
-        printf("Listening on port %d.\n", DEFAULT_CLIENT_PORT);
+    return client_socket;
+}
 
+int main()
+{
+    int client_socket;
+    // initialise connection with the name server
+    init_connection(LOCALHOST, DEFAULT_NS_PORT);
+
+    
 }
--- Client/includes.h
+++ Client/includes.h
@@ -6,5 +6,9 @@
 #include <sys/socket.h>
 #include <netinet/ip.h>
 #include <arpa/inet.h>
+#include <sys/types.h>
+#include <netinet/in.h>
 
 #define DEFAULT_CLIENT_PORT 2109
+#define DEFAULT_NS_PORT 3112
+#define LOCALHOST "127.0.0.1"
