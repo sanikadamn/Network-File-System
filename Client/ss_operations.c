2cf2bd5acc5f07c26159a8aee0ea5c6704544697
aryanchandramania <arashaurchandramania@gmail.com>
2023-11-21 05:12:44 +0530

    add another global

--- Client/ss_operations.c
+++ Client/ss_operations.c
@@ -14,18 +14,18 @@ int ss_read_req(char *action, char *file)
     strcpy(req.action, action);
     strcpy(req.filename, file);
 
-    int len = MAX_ACTION_LENGTH+MAX_FILENAME_LENGTH+20
+    int len = MAX_ACTION_LENGTH+MAX_FILENAME_LENGTH+20;
     char request[len];
 
     sprintf(request, "ACTION:%s\nFILENAME:%s", req.action, req.filename);
 
     // send request to ss
-    if(send(client_ss_socket, request, , 0) < 0)
+    if(send(client_ss_socket, request, len, 0) < 0)
     {
         perror("[-] send error");
         exit(0);
     }
 
     // receive feedback from ss
-    char feedback[MAX_FEEDBACK_LENGTH+20];
+    char feedback[MAX_FEEDBACK_STRING_LENGTH+20];
 }
