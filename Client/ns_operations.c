6b3db8390dcacbb042005f41db4faf2a50a0e161
aryanchandramania <arashaurchandramania@gmail.com>
2023-11-21 17:31:27 +0530

    client: do small fix

--- Client/ns_operations.c
+++ Client/ns_operations.c
@@ -18,11 +18,11 @@
 
 packet_d ns_expect_redirect(char *action, char *file)
 {
+    packet_d rd = {0};
     if(strcasecmp(action, "read") == 0 || strcasecmp(action, "write") == 0 || strcasecmp(action, "info") == 0)
     {
         // expect a redirect packet (type d)
         char *redirect;
-        packet_d rd = {0};
         redirect = read_line(client_ns_socket, MAX_STR_LENGTH+20);
         sscanf(redirect, "STATUS:%d", &rd.status);
 
@@ -41,8 +41,8 @@ packet_d ns_expect_redirect(char *action, char *file)
         // print everything
         printf("NS redirected client to SS at %s:%d\n", rd.ip, rd.port);
         free(redirect);
-        return rd;
     }
+    return rd;
 }
 
 void ns_expect_feedback(char *action, char *file)
@@ -60,8 +60,8 @@ void ns_expect_feedback(char *action, char *file)
         else if(fb.status == 1)
             printf("[+] File created/deleted successfully\n");
         free(feedback);
-        return;
     }
+    return;
 }
 
 // int validate_ns_response(buf_t *response)
